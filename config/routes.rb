be0dfa3e964c576109d5f47f727876f9e0458649
Sawit Pooh <sawit.j@kkumail.com>
2025-08-20 13:41:51 +0700

    feat: redirect to root path when don't have path on website

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   patch "/quests/:id/toggle_status", to: "home#toggle_status", as: :toggle_status_quest
   delete "/quests/:id", to: "home#destroy", as: :quest
   get "/home/brag", to: "home#brag", as: :brag
+  get "*path" => redirect("/")
 end
